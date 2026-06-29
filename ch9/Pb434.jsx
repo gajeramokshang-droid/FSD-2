5ff27b6d4689ba7202bd3874c92e1520dddaf629
Mokshang <gajeramokshang@gmail.com>
2026-06-29 22:06:20 +0530

    Add files via upload

--- ch9/Pb434.jsx
+++ ch9/Pb434.jsx
@@ -1,7 +1,17 @@
 // Write query to perform following tasks on employee collection having fields name, age, position, salary. 
 // 1) Insert 3 documents with random data of having fields name, age, position, salary.
 // 2) Count all employees where age is not equal to 35 and position is “Full Stack Developer”.
-// 3) Update position of all employees to “Software Developer” whose name is “XYZ” and age is equal to 31.
+// 3) Update position of all employees to “Software Developer” whose name is “mokshang” and age is equal to 12.
 //  If not such document available than insert the document with updated values.
 //  4) Display position and name of the employee having lowest salary.
 
+db.mary.insertMany([{name:"mokshang",position:"Backend",age:12,salary:90000},{name:"Thomas",position:"Backend",age:9,salary:100000},{name:"Mercy",posotion:"frontend",age:30,salary:40000}])
+
+
+db.mary.countDocuments({age:{$ne:35},position:"Backend"})
+
+
+db.mary.updateMany({name:"mokshang",age:{$eq:12}},{$set:{position:"FSD"}})
+
+
+db.mary.find().sort({salary:1})
